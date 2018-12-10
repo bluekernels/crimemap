61b9b7b6148c7818472aaae9b4f239525ee110af
bluekernels <operayemy24@gmail.com>
2018-12-10 12:41:11 +0100

    Fix bug in the index.html file

--- crimemap.py
+++ crimemap.py
@@ -1,34 +1,25 @@
 from dbhelper import DBHelper 
 from flask import Flask, render_template, request 
+import json 
 
 app = Flask(__name__)
 db = DBHelper() 
 
 @app.route('/')
-def index(): 
-    try: 
-        data = db.get_all_inputs() 
-    except Exception as e: 
-        print(e)
-        data = None 
-    return render_template('index.html', data=data)
+def index():
+    crimes = db.get_all_crimes() 
+    crimes = json.dumps(crimes)
+    print(crimes)
+    return render_template('index.html', crimes=crimes)
 
-@app.route('/add', methods=['POST'])
-def add(): 
-    try: 
-        data = request.form.get('userinput')
-        db.add_input(data)
-    except Exception as e: 
-        print(e)
+@app.route('/reportcrime', methods=['POST'])
+def reportcrime(): 
+    category = request.form.get('category')
+    date = request.form.get('date')
+    latitude = float(request.form.get('latitude'))
+    longitude = float(request.form.get('longitude'))
+    description = request.form.get('description')
+    db.add_crime(category, date, latitude, longitude, description)
     return index() 
-
-@app.route('/clear')
-def clear(): 
-    try: 
-        db.clear_all() 
-    except Exception as e: 
-        print(e)
-    return index() 
-
 if __name__ == '__main__': 
     app.run(port=4444, debug=True)
